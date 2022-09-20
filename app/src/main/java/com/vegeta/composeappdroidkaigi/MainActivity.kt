7ab28af5e324672af015f44e06740626e7b34015
vegeta2102 <hoangvmh@gmail.com>
2022-09-20 21:56:42 +0900

    Init

---
+++ app/src/main/java/com/vegeta/composeappdroidkaigi/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.vegeta.composeappdroidkaigi
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
+
+
