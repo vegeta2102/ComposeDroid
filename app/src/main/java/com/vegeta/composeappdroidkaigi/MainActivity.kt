f45dac19cb74d7c45fc872a6e5fc3780d7ca413e
vegeta2102 <hoangvmh@gmail.com>
2022-09-23 21:12:32 +0900

    Add column and row

--- app/src/main/java/com/vegeta/composeappdroidkaigi/MainActivity.kt
+++ app/src/main/java/com/vegeta/composeappdroidkaigi/MainActivity.kt
@@ -6,10 +6,12 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -28,16 +30,18 @@ data class Message(val author: String, val body: String)
 
 @Composable
 fun MessageCard(name: Message) {
-    Row() {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
         Image(
             painter = painterResource(id = R.drawable.my_avatar),
             contentDescription = "My avatar",
             modifier = Modifier
-                .width(80.dp)
-                .height(80.dp)
+                .size(80.dp)
+                .clip(CircleShape)
         )
+        Spacer(modifier = Modifier.width(8.dp))
         Column() {
             Text(text = "Hello ${name.author}")
+            Spacer(modifier = Modifier.height(12.dp))
             Text(text = "Hello ${name.body}")
         }
     }
